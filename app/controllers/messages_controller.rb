d3c6599405cb81b86f3a8759ab14dcb7d419b843
Student <example@example.com>
2020-03-10 15:56:49 -0700

    fix weird thing with marking stuff as read

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -16,7 +16,7 @@ class MessagesController < ApplicationController
     end
     if @messages.last
       if @messages.last.user_id != current_user.id
-        @messages.last.read = true;
+        @messages.last.update({read: true})
       end
     end
     @message = @conversation.messages.new
@@ -40,7 +40,7 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:body, :user_id)
+    params.require(:message).permit(:body, :user_id, :read)
   end
 
 end
